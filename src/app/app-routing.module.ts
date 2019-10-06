84c3f0b0f60d020d55772765b2b93b452a021d6a
Krzysztof Kruk <krukkrz@github.com>
2019-10-06 16:54:02 +0200

    logic works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VoterComponent } from './voter/voter.component';
+import { DisplayComponent } from './display/display.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: VoterComponent
+  },
+  {
+    path: 'disp',
+    component: DisplayComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
