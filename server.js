0d7fcd671e38f8c6e8f47b9c1b291cff591cda75
Krzysztof Kruk <krukkrz@github.com>
2019-10-06 21:29:49 +0200

    commit

--- server.js
+++ server.js
@@ -15,3 +15,35 @@ res.sendFile(path.join(__dirname+'/dist/karaoke-client/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
+
+// let express = require('express')
+// let app = express();
+let http = require('http');
+let server = http.Server(app);
+let socketIO = require('socket.io');
+let io = socketIO(server);
+const port = process.env.PORT || 3000;
+
+var counter = 0;
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('vote', (vote) =>{
+        console.log('voted: ', vote);
+        counter += vote;
+        io.emit('vote', counter);
+    })
+
+    socket.on('new-message', (message) => {
+        console.log(message);
+        io.emit('new-message', message);
+    });
+});
+
+
+server.listen(port, () => {
+    console.log(`started on port: ${port}`);
+});
