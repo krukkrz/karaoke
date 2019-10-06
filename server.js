b56ae4e9f6e4e3786ec56e1654089e2b4371db71
Krzysztof Kruk <krukkrz@github.com>
2019-10-06 22:22:51 +0200

    commit

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// let express2 = require('express')
-// let app2 = express2();
-let http = require('http');
-let server = http.Server(app);
-let socketIO = require('socket.io');
-let io = socketIO(server);
-const port = 3000;
+
+// let http = require('http');
+// let server = http.Server(app);
+// let socketIO = require('socket.io');
+// let io = socketIO(server);
+// const port = 3000;
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/karaoke-client'));
@@ -19,29 +18,28 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/karaoke-client/index.html'));
 });
 
-
-
-var counter = 0;
-
-io.on('connection', (socket) => {
-    console.log('user connected');
-    
-    socket.on('vote', (vote) =>{
-        console.log('voted: ', vote);
-        counter += vote;
-        io.emit('vote', counter);
-    })
-    
-    socket.on('new-message', (message) => {
-        console.log(message);
-        io.emit('new-message', message);
-    });
-});
-
 // Start the app by listening on the default Heroku port
-app.listen(8080);
-// app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
 
-server.listen(port, () => {
-    console.log(`started on port: ${port}`);
-});
+
+// var counter = 0;
+
+// io.on('connection', (socket) => {
+//     console.log('user connected');
+    
+//     socket.on('vote', (vote) =>{
+//         console.log('voted: ', vote);
+//         counter += vote;
+//         io.emit('vote', counter);
+//     })
+    
+//     socket.on('new-message', (message) => {
+//         console.log(message);
+//         io.emit('new-message', message);
+//     });
+// });
+
+
+// server.listen(port, () => {
+//     console.log(`started on port: ${port}`);
+// });
